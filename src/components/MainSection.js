ed102db396ba655b3c1bfd0dd182ca0d570fff2b
Andrea Stojkov <andreastojkov00@gmail.com>
2023-01-20 16:09:47 +0100

    Napravljen MainSection(css,js),dodat na App.js

---
+++ src/components/MainSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import './MainSection.css';
+
+function MainSection() {
+  return (
+    <div className='main-container'>
+      <image src='../assets/bckg.jpg'/>
+      <h1>DOMOVI ZDRAVLJA</h1>
+      <p>Brinite o svom zdravlju</p>
+    </div>
+  );
+}
+
+export default MainSection;
